abb055563eb4d4ee806891b9ba20c64c33341b56
Rahul Gupta <rahulgupta4@practo.com>
2018-07-18 10:40:45 +0530

    Copy to clipboard feature add

--- src/components/MarkdownTree.jsx
+++ src/components/MarkdownTree.jsx
@@ -1,16 +1,45 @@
 import React from 'react';
 import treeify from 'treeify';
 import TreeContext from '../contexts/TreeContext.js';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
-function MarkdownTree({ tree }) {
-  const markdown = treeify.asTree(tree['.']);
+class MarkdownTree extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      copiedToClipboard: false,
+    };
+    this.copyToClipboard = this.copyToClipboard.bind(this);
+  }
 
-  return (
-    <pre className="markdown-tree">
-      { markdown }
-    </pre>
-  );
-};
+  copyToClipboard() {
+    this.setState({ copiedToClipboard: true });
+    setTimeout(() => {
+      this.setState({ copiedToClipboard: false });
+    }, 1000);
+  }
+
+  render() {
+    const markdown = treeify.asTree(this.props.tree['.']);
+    const clipboardMarkdown = '```\n' + markdown + '\n```';
+
+    return (
+      <div className="markdown-tree">
+        <CopyToClipboard text={clipboardMarkdown}
+          onCopy={ this.copyToClipboard }>
+          <button disabled={this.state.copiedToClipboard} onClick={ this.copyToClipboard }>
+            { this.state.copiedToClipboard
+              ? <i className="fas fa-check"></i>
+              : 'Copy'
+            }
+          </button>
+        </CopyToClipboard>
+
+        <pre>{ markdown }</pre>
+      </div>
+    );
+  }
+}
 
 export default props => (
   <TreeContext.Consumer>
